ae5d5ff629e03ab343303121af57bfa428a079f9
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-12-13 00:05:43 +0300

    getApi

--- main.js
+++ main.js
@@ -1,21 +1,43 @@
-let input = document.getElementById("weatherinput");
-const submit = document.getElementById("submit");
+let input = document.getElementById('weatherinput')
+const submit = document.getElementById('submit')
+let cityInput = 'Istanbul'
 
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '865ffa770bmsh9b45eb9aa783eb3p1bdde6jsn992cbcb89312',
-		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-	}
-};
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '865ffa770bmsh9b45eb9aa783eb3p1bdde6jsn992cbcb89312',
+    'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+  }
+}
 
-fetch('https://weatherapi-com.p.rapidapi.com/current.json?q=Istanbul', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+submit.addEventListener('click', e => {
+  cityInput = input.value
+  e.preventDefault()
+
+  fetch(
+    `https://weatherapi-com.p.rapidapi.com/current.json?q=${cityInput}`,
+    options
+  )
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err))
+})
+
+console.log(input.value)
 
 
 
-	submit.onclick = () => {
-		console.log(input.value);
-	  };
+
+
+// const village = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '865ffa770bmsh9b45eb9aa783eb3p1bdde6jsn992cbcb89312',
+// 		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://weatherapi-com.p.rapidapi.com/search.json?q=Istanbul', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
